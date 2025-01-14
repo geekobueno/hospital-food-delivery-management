314defbdb1778742740997dc9c649bbf206fac77
GeekoBueno (SIMTAYA Martin) <geeklove2222@gmail.com>
2025-01-14 18:22:56 +0000

    db/prisma/swagger setup

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,59 @@
+import { User } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../enums/role.enum';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty({
+    description: 'User email address',
+    example: 'user@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    description: 'Username for the account',
+    example: 'johndoe',
+    minLength: 3,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  username: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: 'strongPassword123',
+    minLength: 8,
+  })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @ApiProperty({
+    description: 'User role',
+    enum: Role,
+    default: Role.ADMIN,
+    required: true,
+  })
+  @IsEnum(Role)
+  role: Role;
+
+  @ApiProperty()
+  createdAt: Date;
+  @ApiProperty()
+  updatedAt: Date;
+}
+
+//TO-DO : add staff
